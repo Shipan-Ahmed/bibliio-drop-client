7403eee85099bf7a14fbb287a1b771d3653fc758
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-06-28 15:36:01 +0600

    added user reading page

--- src/app/dashboard/reader/delivery-history/page.jsx
+++ src/app/dashboard/reader/delivery-history/page.jsx
@@ -8,36 +8,47 @@ const DeliveryHistoryPage = async () => {
     return (
         <div className='flex flex-col gap-4 container mx-auto min-h-screen'>
             <h2 className="text-2xl font-bold text-neutral">Delivery History</h2>
-            <Table>
-                <Table.ScrollContainer>
-                    <Table.Content aria-label="Team members" className="min-w-[600px]">
-                        <Table.Header>
-                            <Table.Column isRowHeader>Book Title</Table.Column>
-                            <Table.Column>Delivery Fee</Table.Column>
-                            <Table.Column>Transaction ID</Table.Column>
-                            <Table.Column>Request Date</Table.Column>
-                            <Table.Column>Status</Table.Column>
-                        </Table.Header>
-                        <Table.Body>
-                            {
-                                allHistory?.map((book, index) => {
-                                    return (
-                                        <Table.Row key={index}>
-                                            <Table.Cell>{book.title}</Table.Cell>
-                                            <Table.Cell>{book.deliveryfee} $</Table.Cell>
-                                            <Table.Cell>{book.transactionId}</Table.Cell>
-                                            <Table.Cell>{book.requestDate }</Table.Cell>
-                                            <Table.Cell ><div className={`badge  ${book.status === 'pending delivery' ? 'badge badge-primary' : book.status === 'Dispatched' ? 'badge-secondary' : 'badge-success'}`}>{book.status}</div></Table.Cell>
-                                        </Table.Row>
-                                    );
-                                })
-                            }
 
-
-                        </Table.Body>
-                    </Table.Content>
-                </Table.ScrollContainer>
-            </Table>
+            {
+                allHistory.length === 0 ? <>
+                    <div className="bg-white shadow rounded-2xl p-10 text-center border border-gray-100">
+                        <p className="text-gray-500 font-medium">No history yet</p>
+                    </div>
+                </> : <>
+                    <Table>
+                        <Table.ScrollContainer>
+                            <Table.Content aria-label="Team members" className="min-w-[600px]">
+                                <Table.Header>
+                                    <Table.Column isRowHeader>Book Title</Table.Column>
+                                    <Table.Column>Delivery Fee</Table.Column>
+                                    <Table.Column>Transaction ID</Table.Column>
+                                    <Table.Column>Request Date</Table.Column>
+                                    <Table.Column>Status</Table.Column>
+                                </Table.Header>
+                                <Table.Body>
+                                    {
+                                        allHistory?.map((book, index) => {
+                                            return (
+                                                <Table.Row key={index}>
+                                                    <Table.Cell>{book.title}</Table.Cell>
+                                                    <Table.Cell>{book.deliveryfee || book.deliveryFee} $</Table.Cell>
+                                                    <Table.Cell>{book.transactionId}</Table.Cell>
+                                                    <Table.Cell>{book.requestDate}</Table.Cell>
+                                                    <Table.Cell>
+                                                        <div className={`badge ${book.status === 'pending delivery' ? 'badge-primary' : book.status === 'Dispatched' ? 'badge-secondary' : 'badge-success'}`}>
+                                                            {book.status}
+                                                        </div>
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                            );
+                                        })
+                                    }
+                                </Table.Body>
+                            </Table.Content>
+                        </Table.ScrollContainer>
+                    </Table>
+                </>
+            }
         </div>
     );
 };
---
+++ src/app/dashboard/reader/reading-list/page.jsx
@@ -0,0 +1,40 @@
+import { getPaymentUser } from '@/src/lib/action/api';
+import React from 'react';
+import { FiBookOpen } from 'react-icons/fi';
+
+const MyReadingPage = async () => {
+    const allHistory = await getPaymentUser();
+    console.log("Fetched delivery history from API:", allHistory);
+    const ReceivedBooks = allHistory.filter(book => book.status === 'Delivered');
+    return (
+        <div className='flex flex-col gap-4 container mx-auto min-h-screen'>
+            <h2 className="text-3xl font-bold ">My Reading List</h2>
+            <p className="text-gray-600 ">Here are the books I've received</p>
+            {
+                ReceivedBooks.length === 0 ? <>
+                    <div className="bg-white shadow rounded-2xl p-10 text-center border border-gray-100 mt-20 ">
+                        <p className="text-gray-500 font-medium">No history yet</p>
+                    </div>
+                </> : <>
+                    <div className="overflow-x-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                        {
+                              ReceivedBooks?.map((book, index) => {
+                                return (
+                                    <div key={index} className="bg-white shadow rounded-lg p-4 mb-4 flex flex-col items-center justify-center"> 
+                                        <div className="flex items-center justify-center mb-4">
+                                            <FiBookOpen className="text-4xl text-gray-500 mb-2" />
+                                        </div>
+                                        <h3 className="text-2xl font-semibold">{book.title}</h3>
+                                        <p className="text-gray-600">Delivery Status: {book.status}</p>
+                                    </div>   
+                                );
+                              })
+                        }
+                    </div>
+                </>
+            }
+        </div>
+    );
+};
+
+export default MyReadingPage;
